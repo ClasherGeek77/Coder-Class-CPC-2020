c393da57b4536395ffb4918a39b7b3bc6a3bfc45
Kudryavkax <ryusopany@gmail.com>
2020-08-06 14:36:32 +0700

    A. Math added

---
+++ Pemrograman Kompetitif Dasar/src/Math_A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Math_A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger N =new BigInteger(in.next());
+        BigInteger A =new BigInteger(in.next());
+        BigInteger B =new BigInteger(in.next());
+        System.out.println(N.divide(A).add(N.divide(B)).subtract(N.divide(A.multiply(B))));
+    }
+}
