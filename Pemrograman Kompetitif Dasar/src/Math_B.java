c7b97cdb0ae6afb866022a566bb39886f8756248
Kudryavkax <ryusopany@gmail.com>
2020-08-07 20:24:45 +0700

    Comment from Math A to Math C

--- Pemrograman Kompetitif Dasar/src/Math_B.java
+++ Pemrograman Kompetitif Dasar/src/Math_B.java
@@ -9,9 +9,14 @@ public class Math_B {
             System.out.println(0);
             return;
         }
+        // i jadi pecahan (50/100 -> 0.5)
         double i = N/100;
+        // berapa kali i untuk sampe 1, jadi 1/i
         i = 1/i;
 
+        //i itu banyaknya ganjil, diantara 2 ganjil kan ada 1 genap
+        // diantara 3 ganjil ada 2 genap dst.
+        //jadi ini print (ganjil/(genap+ganjil))
         System.out.println((i/(2*i-1)));
 
     }
--- Pemrograman Kompetitif Dasar/src/Math_C.java
+++ Pemrograman Kompetitif Dasar/src/Math_C.java
@@ -6,6 +6,7 @@ public class Math_C {
         Scanner in = new Scanner(System.in);
         BigInteger A =new BigInteger(in.next());
         BigInteger B =new BigInteger(in.next());
+        //print A*B/gcd(A,B)
         System.out.println(A.multiply(B).divide(A.gcd(B)));
     }
 }
