76891966e5f8f2e9c25f4580ac3407df527a4764
Kudryavkax <ryusopany@gmail.com>
2020-08-07 20:18:07 +0700

    Math C Done

---
+++ Pemrograman Kompetitif Dasar/src/Math_C.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Math_C {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger A =new BigInteger(in.next());
+        BigInteger B =new BigInteger(in.next());
+        System.out.println(A.multiply(B).divide(A.gcd(B)));
+    }
+}
